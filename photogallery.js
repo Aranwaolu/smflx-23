7f8c6ce427185b85865fb322e6aa5edf3dce70fe
peace <peacebassey30@gmail.com>
2023-06-23 08:50:53 +0100

    new

--- photogallery.js
+++ photogallery.js
@@ -7,17 +7,9 @@ const openEl = document.querySelector('.burger-menu')
 const closeEl = document.querySelector('.close')
 
 openEl.addEventListener('click', ()=>{
-
-
-
     mobileMenuEl.classList.toggle("open");
-
 })
 
 closeEl.addEventListener('click', ()=>{
-
     mobileMenuEl.classList.remove("open");
-
-
-
 })
